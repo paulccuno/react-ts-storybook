c90c31e8e0a65a0055e7f6b8dfcf3b01cbc2443b
paulccuno <pallccuno@gmail.com>
2023-02-13 23:17:02 -0500

    feat: added semantic-versionaning

---
+++ dist/components/MyLabel.d.ts
@@ -0,0 +1,29 @@
+/// <reference types="react" />
+import './mylabel.css';
+export interface MyLabelProps {
+    /**
+     * Este es el mensaje por defecto del label
+     */
+    label: string;
+    /**
+     * Este es el tamaño por defecto del label
+     */
+    size: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+     * Si quiere todo capitalizado
+     */
+    allCaps?: boolean;
+    /**
+     * Este es el color por defecto del label
+     */
+    color?: 'primary' | 'secondary' | 'tertiary';
+    /**
+     * Este es el color de fuente personalizado del label
+     */
+    fontColor?: string;
+    /**
+     * Este es el color de fondo personalizado del label
+     */
+    backgroundColor?: string;
+}
+export declare const MyLabel: ({ label, size, allCaps, color, fontColor, backgroundColor, }: MyLabelProps) => JSX.Element;
